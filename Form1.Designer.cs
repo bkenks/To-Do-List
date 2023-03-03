1b2bd422ac7306f7b2da52b81c7ab09b9a4c6504
kenkelbt <kenkelbt@mail.uc.edu>
2023-03-02 23:25:01 -0700

    Changed UI to minimal design. Added button.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pnlNav = new System.Windows.Forms.Panel();
+            this.btnToDo = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.panel1.Controls.Add(this.pnlNav);
+            this.panel1.Controls.Add(this.btnToDo);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(186, 577);
+            this.panel1.TabIndex = 0;
+            // 
+            // pnlNav
+            // 
+            this.pnlNav.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.pnlNav.Location = new System.Drawing.Point(0, 193);
+            this.pnlNav.Name = "pnlNav";
+            this.pnlNav.Size = new System.Drawing.Size(3, 100);
+            this.pnlNav.TabIndex = 1;
+            // 
+            // btnToDo
+            // 
+            this.btnToDo.FlatAppearance.BorderSize = 0;
+            this.btnToDo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnToDo.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnToDo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnToDo.Location = new System.Drawing.Point(0, 25);
+            this.btnToDo.Name = "btnToDo";
+            this.btnToDo.Size = new System.Drawing.Size(186, 40);
+            this.btnToDo.TabIndex = 0;
+            this.btnToDo.Text = "To-Do";
+            this.btnToDo.UseVisualStyleBackColor = true;
+            this.btnToDo.Click += new System.EventHandler(this.btnToDo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(800, 577);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Panel panel1;
+        private Panel pnlNav;
+        private Button btnToDo;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,43 @@
+using System.Runtime.InteropServices;
+
 namespace To_Do_List
 {
     public partial class Form1 : Form
     {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,
+            int nTopRect,
+            int nRightRect,
+            int nBottomRect,
+            int nWidthEllipse,
+            int nHeightEllipse
+        );
+
         public Form1()
         {
             InitializeComponent();
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 25, 25));
+            pnlNav.Height = btnToDo.Height;
+            pnlNav.Top = btnToDo.Top;
+            pnlNav.Left = btnToDo.Left;
+            btnToDo.BackColor = Color.FromArgb(46, 51, 73);
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnToDo_Click(object sender, EventArgs e)
+        {
+            pnlNav.Height = btnToDo.Height;
+            pnlNav.Top = btnToDo.Top;
+            pnlNav.Left = btnToDo.Left;
+            btnToDo.BackColor = Color.FromArgb(46, 51, 73);
         }
     }
 }
