d2922994e9f708d05b7c03748058c4ccde7d31a8
kenkelbt <kenkelbt@mail.uc.edu>
2023-03-03 17:28:51 -0700

    Small changes.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,6 +32,8 @@
             this.pnlNav = new System.Windows.Forms.Panel();
             this.btnToDo = new System.Windows.Forms.Button();
             this.btnClose = new System.Windows.Forms.Button();
+            this.lblTasks = new System.Windows.Forms.Label();
+            this.chkTasks = new System.Windows.Forms.CheckedListBox();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -42,18 +44,16 @@
             this.panel1.Controls.Add(this.btnToDo);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(213, 769);
+            this.panel1.Size = new System.Drawing.Size(186, 577);
             this.panel1.TabIndex = 0;
             // 
             // pnlNav
             // 
             this.pnlNav.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.pnlNav.Location = new System.Drawing.Point(0, 257);
-            this.pnlNav.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pnlNav.Location = new System.Drawing.Point(0, 193);
             this.pnlNav.Name = "pnlNav";
-            this.pnlNav.Size = new System.Drawing.Size(3, 133);
+            this.pnlNav.Size = new System.Drawing.Size(3, 100);
             this.pnlNav.TabIndex = 1;
             // 
             // btnToDo
@@ -62,10 +62,9 @@
             this.btnToDo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnToDo.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.btnToDo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.btnToDo.Location = new System.Drawing.Point(0, 33);
-            this.btnToDo.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnToDo.Location = new System.Drawing.Point(0, 25);
             this.btnToDo.Name = "btnToDo";
-            this.btnToDo.Size = new System.Drawing.Size(213, 53);
+            this.btnToDo.Size = new System.Drawing.Size(186, 40);
             this.btnToDo.TabIndex = 0;
             this.btnToDo.Text = "To-Do";
             this.btnToDo.UseVisualStyleBackColor = true;
@@ -77,31 +76,55 @@
             this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnClose.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.btnClose.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.btnClose.Location = new System.Drawing.Point(866, 0);
-            this.btnClose.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnClose.Location = new System.Drawing.Point(758, 0);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(49, 33);
+            this.btnClose.Size = new System.Drawing.Size(43, 25);
             this.btnClose.TabIndex = 2;
             this.btnClose.Text = "X";
             this.btnClose.UseVisualStyleBackColor = true;
             this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
+            // lblTasks
+            // 
+            this.lblTasks.AutoSize = true;
+            this.lblTasks.Font = new System.Drawing.Font("Microsoft Sans Serif", 21F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblTasks.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(161)))), ((int)(((byte)(176)))));
+            this.lblTasks.Location = new System.Drawing.Point(215, 25);
+            this.lblTasks.Name = "lblTasks";
+            this.lblTasks.Size = new System.Drawing.Size(94, 32);
+            this.lblTasks.TabIndex = 3;
+            this.lblTasks.Text = "Tasks";
+            // 
+            // chkTasks
+            // 
+            this.chkTasks.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.chkTasks.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.chkTasks.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.chkTasks.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.chkTasks.FormattingEnabled = true;
+            this.chkTasks.Location = new System.Drawing.Point(215, 60);
+            this.chkTasks.Name = "chkTasks";
+            this.chkTasks.Size = new System.Drawing.Size(550, 432);
+            this.chkTasks.TabIndex = 4;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
-            this.ClientSize = new System.Drawing.Size(914, 769);
+            this.ClientSize = new System.Drawing.Size(800, 577);
+            this.Controls.Add(this.chkTasks);
+            this.Controls.Add(this.lblTasks);
             this.Controls.Add(this.btnClose);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -111,5 +134,7 @@
         private Panel pnlNav;
         private Button btnToDo;
         private Button btnClose;
+        private Label lblTasks;
+        private CheckedListBox chkTasks;
     }
 }
